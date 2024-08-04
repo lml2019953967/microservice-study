347b7064af808697dfb5e2ea1280b25ca600ff7d
dragon <2019953967@qq.com>
2024-08-04 22:41:43 +0800

    microserevice-study version.1

---
+++ inventory-service/src/main/java/com/programming/inventoryservice/model/Inventory.java
@@ -0,0 +1,24 @@
+package com.programming.inventoryservice.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "t_inventory")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+}
